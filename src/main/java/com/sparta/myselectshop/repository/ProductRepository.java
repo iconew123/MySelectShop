6364dd4cf5c7a31cc46014e2e157229ae7826887
iconew123 <iconew123@gmail.com>
2025-02-06 07:45:22 +0900

    feat : add product folder

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product , Long> {
 
-    Page<Product> findAllByUser(User user, Pageable pageable);
+    Page<Product> findAllByUser(User user);
 }
